d589b4de7f0527630c943051f1db25f4897eb09b
LucianMoldovan <1lucianmoldovan@gmail.com>
2019-02-02 13:10:18 +0200

    first draft java work

---
+++ public/javascripts/index.js
@@ -0,0 +1,32 @@
+function loadMembers() {
+    $.ajax("data/members.json").done(function (members) {
+        displayMembers(members);
+        
+    })
+}
+
+function displayMembers(members) {
+    var listMembers = $("table tbody");
+    var rows = members.map(function (member) {
+
+        var a = member.initialSessions;
+        var b = member.usedSessions;
+
+
+        console.info('functioneaza', 'sedinte ramase =', a - b);
+
+        return `<tr>
+        <td>${member.lastName}</td>
+        <td>${member.firstName}</td>
+        <td>${member.initialSessions}</td>
+        <td>${member.usedSessions}</td> 
+        <td>${a-b}</td>
+        </tr>
+        `
+
+    })
+
+document.querySelector("tbody").innerHTML = rows.join('')
+}
+
+loadMembers();
