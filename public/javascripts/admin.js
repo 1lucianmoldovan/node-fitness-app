28c24b80ea03ab1facbc2b668da69881c13fa857
Crisan Razvan <crsn_razvan@yahoo.com>
2019-03-01 12:25:49 +0200

    fixed password validation

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -23,8 +23,7 @@ function displayMembers(members) {
         <td class="tcell" data-id="${member.id}">${member.firstName}</td>
         <td class="tcell" data-id="${member.id}">${a - b}</td>
         <td class="tcell" data-id="${member.id}">${member.endDate}</span></td> 
-        </tr>
-        `
+        </tr>`
     })
 
     document.querySelector("tbody").innerHTML = rows.join('')
@@ -47,20 +46,21 @@ function saveNewMember() {
     var confPassword = $('input[name=confPassword]').val();
     var phone = $('input[name=phone]').val();
     var email = $('input[name=email]').val();
+ 
     //TODO
     var startDate = "0";
     var endDate = "0";
     var initialSessions = "0";
     var usedSessions = "0";
 
-    if (password != confPassword) {
-        document.getElementById('comparePasswords').innerHTML = "Passwords do not match!";
+    if(password != confPassword) {
+        $('#comparePasswords').html("The passwords don't match");
+        return false;
     }
-
-    else if (password.length < 4) {
-        document.getElementById('comparePasswords').innerHTML = "The password must have at least 4 digits!";
+    else if(password.length < 4) {
+        $('#comparePasswords').html("The password must have at least 4 digits!");
+        return false;
     }
-
     else {
         var actionUrl = idToEdit ? 'members/update?id=' + idToEdit : 'members/create';
 
@@ -76,8 +76,9 @@ function saveNewMember() {
         }).done(function (response) {
             idToEdit = "";
             if (response.success) {
-                closeNewMemberForm()
+                // closeNewMemberForm()
                 loadMembers();
+                //TODO close form fadeOut
             }
         })
     }
@@ -97,20 +98,20 @@ function memberSearch() {
 
 function memberEdit() {
     idToEdit = this.getAttribute('data-id');
-    
+
     var member = globalMembers.find(function (member) {
         return member.id == idToEdit;
 
     });
     openNewMemberForm();
-    $('input[name=username]').val(member.username);
-    document.querySelector('input[name=firstName]').value = member.firstName;
+    
+    $('input[name=firstName]').val(member.firstName);
     $('input[name=lastName]').val(member.lastName);
     $('input[name=password]').val(member.password);
     $('input[name=confPassword]').val(member.confPassword);
     $('input[name=phone]').val(member.phone);
     $('input[name=email]').val(member.email);
-    
+
 
     //TODO auto-refresh main-sidebar when finshied to display updated info
 }
@@ -118,21 +119,21 @@ function memberEdit() {
 
 function showMemberDetails() {
     var id = this.getAttribute('data-id');
-    
+
     var member = globalMembers.find(function (member) {
         return member.id == id;
     })
 
-    var editDel =  `<a href="/members/delete?id=${member.id}" title="Delete member" class="edit">&#10006;</a> | 
+    var editDel = `<a href="/members/delete?id=${member.id}" title="Delete member" class="edit">&#10006;</a> | 
                     <a href="#" title="Edit member" class="edit" data-id="${member.id}">&#9998;</a>`
-    
+
     document.querySelector('#delEdit').innerHTML = editDel;
     document.getElementById('userNameDetails').innerHTML = member.username;
     document.getElementById('lastNameDetails').innerHTML = member.lastName;
     document.getElementById('firstNameDetails').innerHTML = member.firstName;
     document.getElementById('phoneDetails').innerHTML = member.phone;
     document.getElementById('emailDetails').innerHTML = member.email;
-   
+
     $('#main-sidebar').animate({
         width: "toggle"
     });
@@ -148,15 +149,18 @@ function initEvents() {
     //member search
     document.getElementById('search').addEventListener('input', memberSearch);
 
+    //save new member
+    $('#submitBtn').click(saveNewMember);
+
     //show member details
     $('tbody').delegate("td.tcell", 'click', showMemberDetails);
-    
-    
+
+
     // member edit 
     $("#main-sidebar").delegate("a.edit", "click", memberEdit);
 }
 
-
+$('.sec-modal').hide();
 $('#main-sidebar').hide();
 initEvents();
 loadMembers();
